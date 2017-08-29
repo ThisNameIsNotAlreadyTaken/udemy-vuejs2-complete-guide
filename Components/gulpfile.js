0ac875a6b44ea02348d6c0a5b3a2a4e2be4a5c72
Vasya <eciseeyou@gmail.com>
2017-08-29 19:01:59 +0300

    Components: introduction

---
+++ Components/gulpfile.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const gulp = require('gulp');
+const debug = require("gulp-debug");
+const runSequence = require("run-sequence");
+const cache = require('gulp-cached');
+const through2 = require('through2');
+const gutil = require('gulp-util');
+const clean = require("gulp-clean");
+const filter = require('gulp-filter');
+const browserify = require('browserify');
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const sourcemaps = require("gulp-sourcemaps");
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const gzip = require('gulp-gzip');
+const watchify = require('watchify');
+const exec = require('child_process').exec;
+const babelify = require('babelify');
+const vueify = require('vueify');
+
+/* clean tasks */
+
+gulp.task("clean:dist", function () {
+    return gulp.src(['./dist/*'], { read: false }).pipe(filter(['**', '!dist/web.config'])).pipe(clean());
+});
+
+gulp.task("clean", function (callback) {
+    runSequence("clean:dist", callback);
+});
+
+/* copy tasks */
+
+gulp.task("html:copy", function () {
+    return gulp.src('./src/index.html').pipe(rename({ dirname: '' })).pipe(gulp.dest("./dist"));
+});
+
+gulp.task("css:copy", function () {
+    return gulp.src('./styles/**/*.css').pipe(rename({ dirname: '' })).pipe(gulp.dest("./dist/styles/"));
+});
+
+gulp.task("fonts:copy", function () {
+    return gulp.src('./fonts/**/*').pipe(rename({ dirname: '' })).pipe(gulp.dest("./dist/fonts/"));
+});
+
+gulp.task("icons:copy", function () {
+    return gulp.src('./src/*.png').pipe(rename({ dirname: '' })).pipe(gulp.dest("./dist/"));
+});
+
+gulp.task("copy", function (callback) {
+    runSequence("html:copy", "css:copy", "fonts:copy", "icons:copy", callback);
+});
+
+/* browserify tasks */
+
+const browserifyOptions = {
+    cache: {},
+    packageCache: {},
+    entries: ['./src/main.js'],
+    debug: true,
+};
+
+function bundleJs(bundle) {
+    return bundle.on("error", function (err) {
+        gutil.log(
+            gutil.colors.red("Browserify compile error:"),
+            err.message
+        );
+        this.emit("end");
+    }).pipe(source("bundle.js"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest('./dist/js/'));
+}
+
+gulp.task('browserify:compile', function () {
+    return bundleJs(browserify(browserifyOptions)
+    .transform("vueify")
+    .transform("babelify", {presets: ["es2015"]})
+    .transform(["require-globify"])
+    .bundle());
+});
+
+gulp.task('browserify:watch', function () {
+    const watchObject = browserify(browserifyOptions)
+    .transform("vueify")
+    .transform("babelify", {presets: ["es2015"]})
+    .transform(["require-globify"])
+    .plugin(watchify);
+
+    function update() {
+        return bundleJs(watchObject.bundle());
+    }
+
+    watchObject.on("update", update);
+    watchObject.on("log", gutil.log);
+
+    return update();
+});
+
+gulp.task("build:dev", function (callback) {
+    runSequence("clean", "copy", "browserify:compile", callback);
+});
+
+gulp.task('dev', function (callback) {
+    runSequence("build:dev", "browserify:watch", callback);
+});
---
+++ Components/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="container">
+        <cmp-header></cmp-header>
+        <hr>
+        <div class="row">
+            <servers></servers>
+            <server-details></server-details>
+        </div>
+        <hr>
+        <cmp-footer></cmp-footer>
+    </div>  
+</template>
+
+<script>
+  import Header from './components/Shared/Header.vue';
+  import Footer from './components/Shared/Footer.vue';
+  import Servers from './components/Server/Servers.vue';
+  import ServerDetails from './components/Server/ServerDetails.vue';  
+
+  export default {
+        components: {
+            'cmp-header': Header,
+            'cmp-footer': Footer,
+            'servers': Servers,
+            'server-details': ServerDetails
+        }
+    }
+</script>
+
+<style>
+</style>
