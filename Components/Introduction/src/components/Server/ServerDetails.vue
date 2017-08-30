8689c154fbfc306d89f30c4e0d5f86264e22ab74
Vasya <eciseeyou@gmail.com>
2017-08-30 16:20:55 +0300

    Component: communication assignment

--- Components/Introduction/src/components/Server/ServerDetails.vue
+++ Components/Introduction/src/components/Server/ServerDetails.vue
@@ -1,11 +1,34 @@
 <template>
     <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+        <p v-if="!server">Server Details are currently not updated</p>
+        <div v-if="server">
+            <p>Server id: {{ server.id }}</p>
+            <p>Server status: {{ server.status }}</p>
+            <button @click="setStatusToNormal">Set status to normal</button>
+        </div>
     </div>
 </template>
 
 <script>
+    import { eventBus } from '../../main';
+
+    export default {
+        data() {
+            return { server: null }
+        },
+        methods: {
+            setStatusToNormal() {
+                this.server.status = 'Normal';
+            }
+        },
+        created() {
+            eventBus.$on('onDetailsShow', (data) => {
+                this.server = data;
+            });
+        }
+    }
 </script>
+
 
 <style scoped>
     div {
--- Components/Introduction/src/components/Server/Servers.vue
+++ Components/Introduction/src/components/Server/Servers.vue
@@ -1,18 +1,46 @@
 <template>
     <div class="col-xs-12 col-sm-6">
         <ul class="list-group">
-            <li class="list-group-item" v-for="index in 5" v-bind:key="index">
-                Server #{{ index }}
+            <li class="list-group-item" v-for="server in servers" v-bind:key="server.id" @click="showDetails(server)">
+                Server #{{ server.id }} status={{ server.status }}
             </li>
         </ul>
     </div>
 </template>
 
 <script>
+    import { eventBus } from '../../main';
+
+    export default {
+        data() {
+            return {
+                servers: [
+                    { id: 1, status: 'Normal' },
+                    { id: 2, status: 'Critical' },
+                    { id: 3, status: 'Unknown' },
+                    { id: 4, status: 'Normal' }
+                ]
+            };
+        },
+        methods: {
+            showDetails(server) {
+                eventBus.$emit('onDetailsShow', server);
+            }
+        }
+    }
 </script>
+
 
 <style scoped>
     div {
         border: 1px solid blue;
+    }
+
+    li {
+        cursor: pointer;
+    }
+
+    li:hover {
+        background-color: lightgrey;
     }
 </style>
