c03d4ceb5b55c9e20a3ac609b0f57e6b6eb3cd90
Vasya <eciseeyou@gmail.com>
2017-09-03 01:37:52 +0300

    Directives

---
+++ Directives/src/Assignment.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives exercise</h1>
+                <button v-myon="clicked">Click me</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            clicked() {
+                alert('Clicked');
+            }
+        },
+        directives: {
+            'myon': {
+                bind(el, binding, vnode) {
+                    el.addEventListener('click', () => {
+                        binding.value();
+                    });
+                }
+            }
+        }
+    }
+</script>
