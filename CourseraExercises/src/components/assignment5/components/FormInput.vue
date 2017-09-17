2672a6716b20ba535ca764e8ef79a960c5cbe955
Vasya <eciseeyou@gmail.com>
2017-09-17 20:57:10 +0300

    Coursera assignment 5 - little changes

--- CourseraExercises/src/components/assignment5/components/FormInput.vue
+++ CourseraExercises/src/components/assignment5/components/FormInput.vue
@@ -2,7 +2,8 @@
     <div class="form-group has-feedback" :class="{ 'has-success': !$v.$invalid, 'has-error': $v.$dirty && $v.$invalid }">
         <label class="col-sm-2 control-label">{{ name }}</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" :placeholder="placeholder || name" @input="$emit('update:value', value)" v-model="value" @blur="$v.$touch()">
+            <input type="text" class="form-control" :placeholder="placeholder || name" @input="$emit('update:value', $event.target.value)" 
+                :value="value" @blur="$v.$touch()">
             <span class="glyphicon glyphicon-ok form-control-feedback" v-if="!$v.$invalid" aria-hidden="true"></span>
             <span class="glyphicon glyphicon-remove form-control-feedback" v-if="$v.$dirty && $v.$invalid" aria-hidden="true"></span>
         </div>
